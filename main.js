d52734eeb351e1ee3c69c3d9bf62145056084ed3
Crave Asia <CraveAsia@syafiqah-mac-mini.local>
2026-01-13 15:56:14 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("Kampung Chaos initialized!");
+
+// Cards are now anchor tags - no JS needed for navigation
+// This file can be used for future enhancements (sound effects, particles, etc.)
